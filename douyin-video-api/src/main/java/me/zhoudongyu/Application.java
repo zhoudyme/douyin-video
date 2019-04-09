a11689771d98cf3650468f838587cfce67729509
Steve <zhoudongyu08@gmail.com>
2019-04-09 21:16:34 +0800

    初始化项目，采用springboot和maven分层

---
+++ douyin-video-api/src/main/java/me/zhoudongyu/Application.java
@@ -0,0 +1,16 @@
+package me.zhoudongyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Steve
+ * @date 2019/04/03
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
