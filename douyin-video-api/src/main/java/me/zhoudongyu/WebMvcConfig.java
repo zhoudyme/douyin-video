a7621a3aa8c6adb4aeda7a5710bfd13ab1da0968
Steve <zhoudongyu08@gmail.com>
2019-06-19 22:22:14 +0800

    1、新增拦截器，完善登录拦截 2、加入Json工具类

--- douyin-video-api/src/main/java/me/zhoudongyu/WebMvcConfig.java
+++ douyin-video-api/src/main/java/me/zhoudongyu/WebMvcConfig.java
@@ -1,6 +1,9 @@
 package me.zhoudongyu;
 
+import me.zhoudongyu.controller.interceptor.MiniInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -18,10 +21,24 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
      */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/META-INF/resources/")
                 .addResourceLocations("file:fileSpace/");
 
     }
+
+    @Bean
+    public MiniInterceptor miniInterceptor() {
+        return new MiniInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(miniInterceptor())
+                .addPathPatterns("/user/**")
+                .addPathPatterns("/video/upload", "/video/uploadCover")
+                .addPathPatterns("/bgm/**");
+        super.addInterceptors(registry);
+    }
+
 }
---
+++ douyin-video-api/src/main/java/me/zhoudongyu/controller/interceptor/MiniInterceptor.java
@@ -0,0 +1,88 @@
+package me.zhoudongyu.controller.interceptor;
+
+import me.zhoudongyu.utils.JSONResult;
+import me.zhoudongyu.utils.JsonUtils;
+import me.zhoudongyu.utils.RedisOperator;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * @author Steve
+ * @date 2019/06/18
+ */
+public class MiniInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private RedisOperator redis;
+
+    private static final String USER_REDIS_TOKEN = "user-redis-token";
+
+    /**
+     * 拦截请求，在controller调用之前
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object o) throws Exception {
+        String userId = request.getHeader("userId");
+        String userToken = request.getHeader("userToken");
+
+        if (StringUtils.isNoneBlank(userId) && StringUtils.isNoneBlank(userToken)) {
+            String uniqueToken = redis.get(USER_REDIS_TOKEN + ":" + userId);
+            if (StringUtils.isBlank(uniqueToken)) {
+                System.out.println("请登录...");
+                returnErrorResponse(response, JSONResult.errorTokenMsg("请登录..."));
+                return false;
+            } else {
+                if (!uniqueToken.equals(userToken)) {
+                    System.out.println("账号被挤出...");
+                    returnErrorResponse(response, JSONResult.errorTokenMsg("账号被挤出..."));
+                    return false;
+                }
+            }
+        } else {
+            System.out.println("请登录...");
+            returnErrorResponse(response, JSONResult.errorTokenMsg("请登录..."));
+            return false;
+        }
+        return true;
+    }
+
+
+    public void returnErrorResponse(HttpServletResponse response, JSONResult result)
+            throws IOException {
+        OutputStream out = null;
+        try {
+            response.setCharacterEncoding("utf-8");
+            response.setContentType("text/json");
+            out = response.getOutputStream();
+            out.write(JsonUtils.objectToJson(result).getBytes("utf-8"));
+            out.flush();
+        } finally {
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+
+    /**
+     * 请求controller之后，渲染视图之前
+     */
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    /**
+     * 请求controller之后，渲染视图之后
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
